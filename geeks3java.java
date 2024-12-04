1e30f839fa981d28f7808f37d2b71a9dc4685801
Neeraj Verma <neerajsingh009424@gmail.com>
2024-12-04 22:49:03 +0530

    updated code

--- geeks3java.java
+++ geeks3java.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class geeks3java {
 
     public static void main(String[] args) {
-        Scanner sc =  new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the size of the array  ");
         int n = sc.nextInt();
 
         int arr[] = new int[n];
         System.out.println(("Enter " + n + "Array "));
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
-
-
 
         }
 
@@ -24,25 +21,15 @@ public class geeks3java {
             arr[left] = arr[right];
             arr[right] = temp;
 
-
             left++;
             right--;
 
-            
         }
 
         System.out.println("Reversed array :");
-        for(int num : arr){
+        for (int num : arr) {
             System.out.println(num + " ");
         }
-
-
-
-
-
-
-
-
 
     }
 }
